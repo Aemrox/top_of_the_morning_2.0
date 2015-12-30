c9db4c0333926818150c251adda7624d4f831614
Aemrox <adam.may2164@gmail.com>
2015-12-30 13:17:22 -0500

    built out channel class, added seeds, and ignored original seeds file

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -3,8 +3,19 @@ class Channel < ActiveRecord::Base
   has_many :stories
   has_many :user_channels
   has_many :users, through: :user_channels
+  include ActiveModel::Validations
 
-
+  def create_story
+    if self.basic
+      story_params = self.site.scrape
+    else #if it is not a basic channel, it must pass a modifier, which is it's downcased name
+      story_params = self.site.scrape(modifier)
+    end
+    new_story = Story.new(story_params)
+    new_story.channel = self
+    new_story.save
+    new_story
+  end
 
   #Basic Channels
   def self.basic_channels #returns an array of all basic channels
@@ -14,5 +25,10 @@ class Channel < ActiveRecord::Base
   def basic! #toggles a channel as basic or not
     self.basic ? self.basic = false : self.basic = true
     self.save
+  end
+
+  private
+  def modifier
+    self.name.downcase
   end
 end
