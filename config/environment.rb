c94596f2f1012c3d6ebc60c285de07ef392cd91a
Aemrox <adam.may2164@gmail.com>
2016-01-03 13:10:03 -0500

    forgot to include production group in environment, fixed

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,21 @@
 require 'bundler'
-Bundler.require(:default, :development)
+Bundler.require(:default, :development, :production)
 
 configure :development do
 	set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
 end
 
 configure :production do
-	set :database, {adapter: "activerecord-postgresql-adapter", database: ENV['DATABASE_URL'] || 'sqlite://database.db}
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
 end
 
 require_relative '../app/controllers/application_controller.rb'
