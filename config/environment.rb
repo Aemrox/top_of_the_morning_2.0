6f1067477942d2815dc44638f4fa4d4709ee19df
Aemrox <adam.may2164@gmail.com>
2016-01-04 09:35:26 -0500

    saving changes to try and delete a phantom db

--- config/environment.rb
+++ config/environment.rb
@@ -1,23 +1,16 @@
 require 'bundler'
 Bundler.require(:default, :development, :production)
 
-configure :development do
- set :database, 'sqlite:///dev.db'
- set :show_exceptions, true
-end
+db = URI.parse('postgres://user:pass@localhost/dbname')
 
-configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
-
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
-end
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'unicode'
+)
 
 require_relative '../app/controllers/application_controller.rb'
 require_all 'app/models'
